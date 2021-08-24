8a8abb0c7ccb426c95c15d865f539c387b8a7618
anjalishinde1912 <anjalishinde1912@gmail.com>
2021-08-24 13:33:20 +0530

    Css grid

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,7 +10,7 @@ export class CalculatorComponent {
   numberGroups =[1,2,3,'+'];
   numberGroups1 =[4,5,6,'-'];
   numberGroups2 =[7,8,9,'*'];
-  numberGroups3 =[0,'%','=','c'];
+  numberGroups3 =[0,'/','=','c'];
   readyForNewInput = true;
   oldvalue='';
   lastoperator='*';
@@ -27,12 +27,12 @@ export class CalculatorComponent {
         this.value = '' + num;
         else
         this.value += '' + num;
-        this. readyForNewInput=false;
+        this.readyForNewInput=false;
       }
       
       else if(num ==="c") {
         this.value = '0';
-        this. readyForNewInput=true;
+        this.readyForNewInput=true;
       }
 
       else if (num==='=') 
@@ -45,9 +45,9 @@ export class CalculatorComponent {
         {
           this.value ='' + (parseInt(this.value) + parseInt(this.oldvalue));
         }
-        else if(this.lastoperator==="%")
+        else if(this.lastoperator==="/")
         {
-         this.value = '' + (parseInt(this.oldvalue) % parseInt(this.value));
+         this.value = '' + (parseInt(this.oldvalue) /parseInt(this.value));
         }
          else if(this.lastoperator==="-")
         {
